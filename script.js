df33b220c7013986f9faedde26343a67c701c112
Sushant Kumar <sushantkumar9378@gmail.com>
2026-04-03 23:08:26 +0530

    hover effect on each column

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 const todoColumn = document.querySelector('#todo');
 const progressColumn = document.querySelector('#progress');
 const doneColumn = document.querySelector('#done');
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener('drag', (e) => {
+        console.log('Dragging', e);
+      })
+    })
+
+     function addDragEventsOnColumn(column) {
+        column.addEventListener('dragenter', (e) => {
+            e.preventDefault();
+            column.classList.add('hover-over');
+        })
+        column.addEventListener('dragleave', (e) => {
+            e.preventDefault();
+            column.classList.remove('hover-over');
+        })
+        column.addEventListener('dragover', (e) => {
+            e.preventDefault();     
+           })
+
+
+
+
+        column.addEventListener('drop', (e) => {
+            e.preventDefault();
+            console.log('Dropped', e);
+        })
+    }
+
+    addDragEventsOnColumn(todo);
+    addDragEventsOnColumn(progress);
+    addDragEventsOnColumn(done);
